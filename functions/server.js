dfc83e0767ffc8ffec3462034e437e88b330cf1f
Ronald K <2fixelectronics777@gmail.com>
2025-04-17 23:46:53 -0500

    error fix mod

--- functions/server.js
+++ functions/server.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, '../public')));
 // Handlebars setup
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, '../views'));
+app.set('views', path.resolve(__dirname, '../views'));
 
 // MongoDB connection
 mongoose
